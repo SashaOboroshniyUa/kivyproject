63d3c31776c218b801267b9f4468e3530d889f7c
sigma papa <oborozbujsasa@gmail.com>
2024-08-08 16:15:30 +0300

    last commit

--- Original_Kivy_Project.py
+++ Original_Kivy_Project.py
@@ -6,6 +6,8 @@ from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 
 Window.size = (1920, 1080)
+
+
 class FinanceManagerApp(App):
 
     def build(self):
@@ -16,20 +18,21 @@ class FinanceManagerApp(App):
         input_layout = BoxLayout(orientation='horizontal', spacing=10, size_hint=(1, None), height=40)
         self.sum_input = TextInput(hint_text='Сума', multiline=False, input_type='number')
         self.source_input = TextInput(hint_text='Джерело', multiline=False)
-        self.sourc_input = TextInput(hint_text='Категорія', multiline=False)
+        self.category_input = TextInput(hint_text='Категорія', multiline=False)
         input_layout.add_widget(self.sum_input)
         input_layout.add_widget(self.source_input)
-        input_layout.add_widget(self.sourc_input)
+        input_layout.add_widget(self.category_input)
         add_income_button = Button(text='Добавити прибуток', size_hint=(1, None), height=40)
         add_income_button.bind(on_press=self.add_income)
         add_button = Button(text='Добавити видаток', size_hint=(1, None), height=40)
         add_button.bind(on_press=self.add_expense)
-        self.balance_label = Label(text=f'Баланс: {self.balance}', size_hint=(1, 1), halign='center',
-                                   valign='middle')
+        self.balance_label = Label(text=f'Баланс: {self.balance}', size_hint=(1, 1), halign='center', valign='middle')
         root.add_widget(input_layout)
         root.add_widget(add_income_button)
         root.add_widget(add_button)
         root.add_widget(self.balance_label)
+        self.category_layout = BoxLayout(orientation='vertical', spacing=10, size_hint=(0.2, 1), padding=10)
+        root.add_widget(self.category_layout)
 
         return root
 
@@ -37,25 +40,43 @@ class FinanceManagerApp(App):
         try:
             amount = float(self.sum_input.text)
             source = self.source_input.text
-            self.incomes.append({'sum': amount, 'source': source})
+            category = self.category_input.text
+            self.incomes.append({'sum': amount, 'source': source, 'category': category})
             self.balance += amount
             self.update_balance_label()
+            self.update_category_buttons()
         except ValueError:
             pass
 
     def add_expense(self, instance):
         try:
             amount = float(self.sum_input.text)
-            category = self.source_input.text
+            category = self.category_input.text
             if self.balance - amount >= 0:
                 self.expenses.append({'sum': amount, 'category': category})
                 self.balance -= amount
                 self.update_balance_label()
+                self.update_category_buttons()
         except ValueError:
             pass
 
     def update_balance_label(self):
-        self.balance_label.text = f'Текущий баланс: {self.balance}'
+        self.balance_label.text = f'Баланс: {self.balance}'
+
+    def update_category_buttons(self):
+        categories = []
+        for expense in self.expenses:
+            if expense['category'] not in categories:
+                categories.append(expense['category'])
+
+        for income in self.incomes:
+            if income['category'] not in categories:
+                categories.append(income['category'])
+
+        self.category_layout.clear_widgets()
+
+        for category in categories:
+            self.category_layout.add_widget(Button(text=category, size_hint_y=None, height=40))
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,71 +1,8 @@
-![taxi-service logo](readme.images/home.png)
-
-# taxi-service
+# Инструкция по активации
 ***
-This is a simple application made for educational and demonstration purposes. Implements 
-authentication by filters based on session. Logs some Authentication information and create/update/delete database operations to console and ```.log``` file. Registration of new drivers, managing the relations between cars, drivers and manufacturers and other CRUD operations.  
-Program has following functionalities:
-- create new Manufacturer
-- display all Manufacturers
-- create new Driver
-- display all Drivers
-- create new Car
-- display all Cars
-- add driver to car
-- list all cars by driver
-
-You can access all of them from the main ```/index``` page  
-after registering as a new driver at ```/drivers/add``` page  
-and signing in at ```/drivers/login``` page (to which you will be redirected if you will try to access pages that are not allowed for unsigned drivers).  
-After that you will have access to all the pages and functionality.
-
-## Screenshots
-![screenshot1](readme.images/screenshots/1.png)
-![screenshot2](readme.images/screenshots/2.png)
-![screenshot3](readme.images/screenshots/3.png)
-
----
-## Architecture
-1. DAO - Data access layer
-2. Service - Application logic layer
-3. Controllers - Presentation layer
----
-## Technologies
-- Java 11
-- Apache Maven
-- Apache TomCat
-- Apache Log4j2
-- MySQL
-- JDBC
-- Http Servlet
-- JSTL
-- JSP
-- HTML, CSS, XML, Bootstrap
-
----
-## How to run a project
-*WARNING* Installed TomCat and MySQL is compulsory for this project.
-1. Add a tomcat local configuration in IntelliJ (Tomcat server - local, deployment - war 
-exploded, application context: /)
-2. Run SQL script located in ```src/main/resources/init_db.sql``` to set up a database for this project.
-3. Configure ```src/main/java/taxi/util/ConnectionUtil.java``` with your ```URL```, ```USERNAME```, ```PASSWORD``` and ```JDBC_DRIVER```.
-4. In the ```src/main/resources/log4j2.xml``` at line 7 you also need to set ```ABSOLUTE_PATH``` to your ```.log``` file.
-5. Import Maven dependency from pom.xml.
-
-
----
-## Demo
-https://salty-citadel-80806.herokuapp.com/
-
-Login: root
-
-Password: 1234
-
----
-## Your database should look like this
-
-![shema DB](readme.images/taxi-database-image.png?raw=true "Database")
-
----
-## Contacts
-My [LinkedIn](https://www.linkedin.com/)  
+Что бы запустить мой не до проект нужно сделать следующие вещи:
+- Скачать PyCharm
+- открыть терминал слева снизу
+- Вписать команду .\.venv\Scripts\activate
+- После вписать pip install kivy и pip install kivy[base] kivy_examples --pre --extra-index-url https://kivy.org/downloads/simple/
+- После нужно вставить мой код и запустить
